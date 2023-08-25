b77cc165cb81e06a9ff156aef80f3972f00e60b4
hwingb <71150938+hwingb@users.noreply.github.com>
2023-08-25 21:44:06 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plist17lib',
+      version='0.0.1',
+      description='Python library to parse Apple\'s bplist17 format into JSON',
+      author='hwingb, dserd',
+      author_email='',
+      url='https://github.com/hwingb/plist17lib',
+      packages=['plist17lib'],
+     )
