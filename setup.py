ef9eb27b567decc1770ca570b51658d03c962400
hwingb <71150938+hwingb@users.noreply.github.com>
2023-09-27 16:08:46 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='plist17lib',
-      version='0.0.1',
+      version='0.0.2',
       description='Python library to parse Apple\'s bplist17 format into JSON',
       author='hwingb, dserd',
       author_email='',
